da9f47fb4b775622140e7bb1fc0f39cf5f196e00
Faisal <pros3sa@gmail.com>
2025-04-03 22:38:10 +0000

    starting mobile responsiveness

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ let limit = document.getElementById("limit");
 Limitcheckbox.onchange = function() {
     limit.style.display = this.checked ? "block" : "none";
 }
-console.log(Limitcheckbox.checked);
+
+const themeToggle = document.getElementById('theme-toggle');
+
+// Set initial theme based on user's preference
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark-mode');
+}
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
